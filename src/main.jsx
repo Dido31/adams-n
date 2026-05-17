05981c83640d98494d2d6e38c66bcf25ebd099a5
Dido31 <orhan.emirgnc@gmail.com>
2026-05-17 19:08:13 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontFamily: 'sans-serif' }}>
+      <h1>Jarvis Başarıyla Çalışıyor!</h1>
+    </div>
+  </React.StrictMode>
+)
